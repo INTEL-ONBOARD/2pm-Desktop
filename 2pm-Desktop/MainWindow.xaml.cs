90847329bbe0c1855e23a932e58e81ebd50bd8c1
INTEL-ONBOARD <75471327+INTEL-ONBOARD@users.noreply.github.com>
2024-09-20 23:59:50 +0530

    fixes - layout adjested

--- 2pm-Desktop/MainWindow.xaml.cs
+++ 2pm-Desktop/MainWindow.xaml.cs
@@ -20,5 +20,11 @@ namespace _2pm_Desktop
         {
             InitializeComponent();
         }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+        }
     }
 }

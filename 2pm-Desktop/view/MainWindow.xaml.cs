6858b12837f0fc002685aa87e64d90ac991fd28a
INTEL-ONBOARD <75471327+INTEL-ONBOARD@users.noreply.github.com>
2024-09-21 02:17:55 +0530

    loading screen UI fixed + connection validation

--- 2pm-Desktop/view/MainWindow.xaml.cs
+++ 2pm-Desktop/view/MainWindow.xaml.cs
@@ -1,30 +1,91 @@
-﻿using System.Text;
+﻿using System;
+using System.Net.NetworkInformation;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _2pm_Desktop
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
         }
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
-            this.Left = desktopWorkingArea.Right - this.Width;
-            this.Top = desktopWorkingArea.Bottom - this.Height;
+            var workingArea = System.Windows.SystemParameters.WorkArea;
+
+            double left = workingArea.Right - this.Width - 10;
+            double top = workingArea.Bottom - this.Height - 10;
+
+            this.Left = left;
+            this.Top = top;
+
+            do
+            {
+                try
+                {
+                    Ping myPing = new Ping();
+                    string host = "www.nibmworldwide.com";
+                    byte[] buffer = new byte[32];
+                    int timeout = 1000;
+                    PingOptions pingOptions = new PingOptions();
+                    PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                    System.Diagnostics.Debug.WriteLine("done");
+                    status.Visibility = Visibility.Visible;
+                    status.Content = "Checking Connection...";
+                    await Task.Delay(3000);
+
+                    status.Visibility = Visibility.Hidden;
+                    loadingScreen.Visibility = Visibility.Collapsed;
+                    loginScreen.Visibility = Visibility.Visible;
+                    await Task.Delay(1000);
+
+                    break;
+                }
+                catch (Exception)
+                {
+                    loadingScreen.Visibility = Visibility.Visible;
+                    status.Visibility = Visibility.Visible;
+                    status.Content = "Something went wrong... Try again!";
+                }
+            } while (true);
+        }
+
+        private async void validNetwork(object sender, RoutedEventArgs e)
+        {
+            do
+            {
+                try
+                {
+                    Ping myPing = new Ping();
+                    string host = "www.nibmworldwide.com";
+                    byte[] buffer = new byte[32];
+                    int timeout = 1000;
+                    PingOptions pingOptions = new PingOptions();
+                    PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                    System.Diagnostics.Debug.WriteLine("done");
+                    status.Visibility = Visibility.Visible;
+                    status.Content = "Checking Connection...";
+                    await Task.Delay(3000);
+
+                    status.Visibility = Visibility.Hidden;
+                    loadingScreen.Visibility = Visibility.Hidden;
+                    loginScreen.Visibility = Visibility.Visible;
+
+                    await Task.Delay(1000);
+
+                    break;
+                }
+                catch (Exception)
+                {
+                    loadingScreen.Visibility = Visibility.Hidden;
+                    status.Visibility = Visibility.Visible;
+                    status.Content = "Something went wrong... Try again!";
+                }
+            } while (true);
+
         }
     }
 }
